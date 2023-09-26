92e9fdd2ffddef347b1aad386c0776db04166e74
Sahil Thapa <thapasahil997@gmail.com>
2023-09-26 13:05:30 +0300

    async await testing

--- __tests__/carstorageFilePromisesTest.js
+++ __tests__/carstorageFilePromisesTest.js
@@ -3,14 +3,19 @@
 const {search}=require('../carstorageFilePromises');
 
 describe('Testing with key "licence" and value "ABC-1" to resolve', () => {
-    const expected=[
+    const result=[
         {
             "model":"Bored T-model","licence":"ABC-1"
         }
     ];
     test('then', () => {
         return search('licence','ABC-1')
-        .then(data=>expect(data).toEqual(expected))
+        .then(data=>expect(data).toEqual(result))
     });
+
+    test('async-await',async ()=>{
+        const data= await search('licence','ABC-1');
+        expect(data).toEqual(result);
+    })
     
 });
